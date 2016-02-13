5145c6396d9fbe6b807335cd91471245b8842e64
rholmes69 <roderick.sholmes@gmail.com>
2016-02-13 12:16:07 -0800

    beutify code

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/book', function(req, res, next) {
+  res.send('respond with a book resource');
+});
+
 module.exports = router;
